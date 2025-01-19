763659be204cf6bf9cb89cb3d1e73476a25ddb2f
Oliver Mao <olivermao2005@gmail.com>
2025-01-18 21:39:20 -0500

    slight styling to topbar

--- depolarizer/src/pages/TopBar.jsx
+++ depolarizer/src/pages/TopBar.jsx
@@ -5,29 +5,26 @@ import {
     Typography
 } from '@mui/material';
 
-function TopBar({children}) {
+function TopBar({ children }) {
     const signIn = () => {
         alert("Sign In (Placeholder)");
     }
     
     return (
         <div className="h-full w-full">
-            <div className="absolute flex w-full flex-row">
-                <Typography
-                    className="flex-grow-0 flex"
-                    style={{fontSize: "calc(20px + 4vmin)"}}
-                >
-                    Depolarizer
-                </Typography>
+            <div className="mt-30 ml-30 flex w-full flex-row">
+                <Typography color="info" style={{fontSize: "calc(20px + 4vmin)"}}>De</Typography>
+                <Typography color="error" style={{fontSize: "calc(20px + 4vmin)"}}>polar</Typography>
+                <Typography color="info" style={{fontSize: "calc(20px + 4vmin)"}}>izer</Typography>
                 <Button
-                    className="ml-auto right-0"
+                    className="ml-auto right-0 h-12"
                     variant="contained"
                     onClick={signIn}
                 >
                     Sign In
                 </Button>
             </div>
-            <children/>
+            {children}
         </div>
     );
   }
